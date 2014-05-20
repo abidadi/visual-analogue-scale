a0ddea6dd7d92af081bb9f8d867e2e566fe90ddd
Azhar Khan <azmanian.devil@gmail.com>
2014-05-20 00:01:25 -0400

    CSV exporting for each verb

--- src/survey/admin.py
+++ src/survey/admin.py
@@ -1,31 +1,33 @@
+import csv
+import StringIO
+
+from django.http import HttpResponse
 from django.contrib import admin
+
 from survey.models import Survey, Verbs
 
 class VerbsInline(admin.TabularInline):
     model = Verbs
     extra = 3
 
-class SurveyAdmin(admin.ModelAdmin):   
+class SurveyAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,               {'fields': ['name']}),
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
     list_display = ('name', 'pub_date')
     inlines = [VerbsInline]
-    
-    actions = ['download_csv']
-    
-    def download_csv(ModelAdmin, request, queryset):
-        import csv
-        from django.http import HttpResponse
-        import StringIO
 
+    actions = ['download_csv']
+
+    def download_csv(ModelAdmin, request, queryset):
         f = StringIO.StringIO()
         writer = csv.writer(f)
-        writer.writerow(["name", "pub_date"])
+        writer.writerow(["name", "value", "N/A", "survey"])
 
         for s in queryset:
-        	writer.writerow([s.name, s.pub_date])
+            for verb in s.verbs_set.all():
+                writer.writerow([verb.verb_text, verb.verb_scale, verb.verb_NA, verb.survey.name])
 
         f.seek(0)
         response = HttpResponse(f, content_type='text/csv')
@@ -33,5 +35,5 @@ class SurveyAdmin(admin.ModelAdmin):
         return response
 
     download_csv.short_description = "Download CSV"
-    
+
 admin.site.register(Survey, SurveyAdmin)
