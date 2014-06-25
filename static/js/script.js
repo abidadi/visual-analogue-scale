7b2be110f32183434703856f41339ffc606de2b2
abidadi <Ashish@Ashish-Bidadis-MacBook-Pro.local>
2014-06-25 16:17:15 -0400

    Moved script.js file, edited config files

--- static/js/script.js
+++ static/js/script.js
@@ -3,7 +3,8 @@ $("form").submit(function(event) {
 
     $('form input[type="range"]').each(function() {
         if(!($(this)[0].disabled) && ($(this)[0].value == 0)) { // if value is blank and input is not disabled, then form is invalid
-            alert('Please evaluate or check "N/A" for all verbs. ' + $(this)[0].name + ' currently blank.');
+            alert('The rating form is incomplete. Please evaluate or check "N/A" for all verbs.');
+            //alert('Please evaluate or check "N/A" for all verbs. ' + $(this)[0].name + ' currently blank.');
             event.preventDefault();
             return false;
         }
