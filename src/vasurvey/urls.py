0a1d70666d053b54b09e200fd5d6798def77917b
abidadi <Ashish@Ashish-Bidadis-MacBook-Pro.local>
2014-06-28 15:29:22 -0400

    Added redirect for base url to show survey list

--- src/vasurvey/urls.py
+++ src/vasurvey/urls.py
@@ -11,6 +11,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'vaSurvey.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^', include('survey.urls', namespace="survey")),
     url(r'^survey/', include('survey.urls', namespace="survey")),
     url(r'^admin/', include(admin.site.urls)),
 )
